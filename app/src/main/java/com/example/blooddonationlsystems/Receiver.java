93fbfcf44eef50d2892a9f2c8fdd4c476e7688b7
Kaushi <it20186524@my.sliit.lk>
2021-10-03 20:05:58 +0700

    Receiver Model,Receiver blood java files and Receiver blood xml file implement

---
+++ app/src/main/java/com/example/blooddonationlsystems/Receiver.java
@@ -0,0 +1,110 @@
+package com.example.blooddonationlsystems;
+
+import com.google.firebase.database.Exclude;
+
+public class Receiver {
+    private String nationalId;
+    private String name;
+    private String bloodType;
+    private String gender;
+    private String dob;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String password;
+
+    @Exclude
+    private String key;
+
+    public Receiver() {
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
