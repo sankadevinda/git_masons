d87c07415f25b002a01a03ae81cc329095318f8c
Oshadhini <it20193768@my.sliit.lk>
2021-10-04 11:05:43 -0700

    Avilableblood listxml implemented

---
+++ app/src/main/java/com/example/blooddonationlsystems/AvailabilityListSource.java
@@ -0,0 +1,14 @@
+package com.example.blooddonationlsystems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvailabilityListSource extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_availability_list_source);
+    }
+}
--- app/src/main/java/com/example/blooddonationlsystems/availabilityofblood.java
+++ app/src/main/java/com/example/blooddonationlsystems/availabilityofblood.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Avilabilityofblood extends AppCompatActivity {
+public class availabilityofblood extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_avilabilityofblood);
+        setContentView(R.layout.activity_availabilityofblood);
     }
 }
