7a0f4868ac539a1c62cad7a21a8e6dcb11cbafd9
sanka <kvsankadevinda@gmail.com>
2021-10-05 20:18:27 +0530

    donor profile implimendetd ui

---
+++ app/src/main/java/com/example/blooddonationlsystems/DonorProfile.java
@@ -0,0 +1,14 @@
+package com.example.blooddonationlsystems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_profile);
+    }
+}
